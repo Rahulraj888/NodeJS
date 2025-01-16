62ab5d83015abdb123baa472c3166ffd422e98b7
Rahulraj888 <reddavenirahul@gmail.com>
2025-01-16 18:56:02 -0500

    headers in resst api

--- Backend/index.js
+++ Backend/index.js
@@ -1,37 +1,20 @@
-const http = require('http')
-const fs = require('fs')
-const url = require("url")
+const express = require("express");
+const http = require("http")
 
+const port = 8000;
+const app = express();
 
-const port = 8000
-
-const myServer = http.createServer((req, res) => {
-    if (req.url === "/favicon.ico") return res.end();
-    const log = `${Date.now()}: ${req.method} ${req.url} New req received\n`
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
-
-    fs.appendFile('logs.txt', log, (err, result) => { 
-        switch (myUrl.pathname) {
-            case '/':
-                res.end("Hello from server");
-                break;
-            case '/about':
-                const myName = myUrl.query.name;
-                // console.log(`query parameters are ${myUrl.query.length}`);
-                console.log(`name from query string paramter is ${myName}`);
-                res.end(`Welcome ${myName}`);
-                break;
-            case '/signup':
-                if (req.method === 'GET') res.end("sign up");
-                else if (req.method === 'POST') res.end("success");
-            default:
-                res.end("404 Not Found");
-        }
-    });
+app.get("/", (req, res) => {
+    res.send("Hello from welcome page");
 });
 
+app.get("/about", (req, res) => {
+    console.log(`Query params are there ${req.query}`);
+    res.send("Hello from about page");
+});
 
-myServer.listen(port, () => {
-    console.log(`Server started at port ${port}`);
+// http.createServer(app);
+
+app.listen(port, () => {
+    console.log(`server started on port ${port}`);
 });
--- Project01/index.js
+++ Project01/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const users = require('./MOCK_DATA.json');
+const users = require("./MOCK_DATA.json");
 const fs = require("fs");
 
 const PORT = 8000;
@@ -9,70 +9,72 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    console.log("Middleware 1");
-    next();
+  console.log(req.headers);
+  console.log("Middleware 1");
+  next();
 });
 
 app.use((req, res, next) => {
-    let msg = `${Date.now()}: ${req.path} ${req.method}`;
-    fs.appendFile("logs.txt", msg, (err, data) => {
-        console.log("Middleware 2");
-        next();
-    })
+  let msg = `${Date.now()}: ${req.path} ${req.method}`;
+  fs.appendFile("logs.txt", msg, (err, data) => {
+    console.log("Middleware 2");
+    next();
+  });
 });
 
 app.get("/", (req, res) => {
-    res.send(`Hello World!`);
-})
+  //Always add X to custom headers
+  res.setHeader("X-myName", "Rahul");
+  res.send(`Hello World!`);
+});
 
-app.get("/api/users/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const user = users.find(user => user.id === id);
-    res.json(user);
+app.get("/api/users", (req, res) => {
+  res.setHeader("myName", "Rahul");
+  res.json(users);
 });
 
 app.get("/users", (req, res) => {
-    let html = 
-    `<ul>
-        ${users.map(user => `<li>${user.first_name}</li>`).join("")}
-    </ul>`;
-    
-    res.send(html);
+  let html = `<ul>${users
+    .map((user) => `<li>${user.first_name}</li>`)
+    .join("")}</ul>`;
+  res.send(html);
 });
 
-// different methods on same route
-app.route("/api/users")
-.get((req, res) => {
-    res.json(users);
-})
-// .post((req, res) => {
-//     //TODO -- create post request
-//     return res.json({status:"pending"});
-// })
-.patch((req, res) => {
-    // TODO create patch request
-    return res.json({status:"pending"});
-})
-.delete((req, res) => {
-    // TODO create delete request
-    return res.json({status:"pending"});
-})
 
-// app.get("/api/users/:id", (req, res) => {
-//     const id = Number(req.params.id);
-//     const user = users.find(user => user.id === id);
-//     res.json(user);
-// });
+
+// different methods on same route
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    res.json(user);
+  })
+  .post((req, res) => {
+    //TODO -- create post request
+    return res.json({ status: "pending" });
+  })
+  .patch((req, res) => {
+    // TODO create patch request
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    // TODO create delete request
+    return res.json({ status: "pending" });
+  });
+
+
 
 app.post("/api/users", (req, res) => {
-    const body = req.body;
-    users.push({...body, id: users.length + 1});
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-        if (err) res.json({status: "failed to add user"});
-        return res.json({status:"successful", id: users.length});
-    });
-}); 
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    if (err) res.json({ status: "failed to add user" });
+    return res.json({ status: "successful", id: users.length });
+  });
+});
+
 
 app.listen(PORT, () => {
-    console.log(`Server started at PORT: ${PORT}`);
+  console.log(`Server started at PORT: ${PORT}`);
 });
